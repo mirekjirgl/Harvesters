0c482bbb530c4cc07982d209f556d609456e68a1
mirekjirgl <jirgl@feec.vutbr.cz>
2018-05-16 19:58:26 +0200

    Přidání I2C komunikace

---
+++ EnergyHarvesting/FLASH/Generated_Code/Vectors.d
@@ -0,0 +1,163 @@
+Generated_Code/Vectors.o: ../Generated_Code/Vectors.c \
+ C:/Freescale/CW\ MCU\ v11.0/MCU/ARM_GCC_Support/ewl/EWL_C/include/lib_ewl.prefix \
+ C:/Freescale/CW\ MCU\ v11.0/MCU/ARM_GCC_Support/ewl/EWL_C/include/common.prefix \
+ C:/Freescale/CW\ MCU\ v11.0/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewl_c_version.h \
+ ../Generated_Code/Cpu.h ../Generated_Code/PE_Types.h \
+ C:/Freescale/CW\ MCU\ v11.0/MCU/ARM_GCC_Support/ewl/EWL_C/include/stdint.h \
+ C:/Freescale/CW\ MCU\ v11.0/MCU/ARM_GCC_Support/ewl/EWL_C/include/ansi_parms.h \
+ C:/Freescale/CW\ MCU\ v11.0/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewlGlobals.h \
+ C:/Freescale/CW\ MCU\ v11.0/MCU/ARM_GCC_Support/ewl/EWL_C/include/arm/ansi_prefix.ARM.h \
+ C:/Freescale/CW\ MCU\ v11.0/MCU/ARM_GCC_Support/ewl/EWL_C/include/os_enum.h \
+ C:/Freescale/CW\ MCU\ v11.0/MCU/ARM_GCC_Support/ewl/EWL_C/include/cstdint \
+ C:/Freescale/CW\ MCU\ v11.0/MCU/ARM_GCC_Support/ewl/EWL_C/include/limits_api.h \
+ C:/Freescale/CW\ MCU\ v11.0/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewl_lib_ext1.h \
+ ../Generated_Code/PE_Error.h ../Generated_Code/PE_Const.h \
+ ../Generated_Code/IO_Map.h \
+ D:/Git/Harvesters/EnergyHarvesting/Static_Code/IO_Map/MKL25Z4.h \
+ ../Generated_Code/LED.h \
+ D:/Git/Harvesters/EnergyHarvesting/Static_Code/PDD/GPIO_PDD.h \
+ D:/Git/Harvesters/EnergyHarvesting/Static_Code/PDD/PDD_Types.h \
+ ../Generated_Code/WAIT1.h ../Generated_Code/CsIO1.h \
+ C:/Freescale/CW\ MCU\ v11.0/MCU/ARM_GCC_Support/ewl/EWL_C/include/stdio.h \
+ C:/Freescale/CW\ MCU\ v11.0/MCU/ARM_GCC_Support/ewl/EWL_C/include/cstdio \
+ C:/Freescale/CW\ MCU\ v11.0/MCU/ARM_GCC_Support/ewl/EWL_C/include/cstddef \
+ C:/Freescale/CW\ MCU\ v11.0/MCU/ARM_GCC_Support/ewl/EWL_C/include/null.h \
+ C:/Freescale/CW\ MCU\ v11.0/MCU/ARM_GCC_Support/ewl/EWL_C/include/size_t.h \
+ C:/Freescale/CW\ MCU\ v11.0/MCU/ARM_GCC_Support/ewl/EWL_C/include/wchar_t.h \
+ C:/Freescale/CW\ MCU\ v11.0/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewl_rsize_t.h \
+ C:/Freescale/CW\ MCU\ v11.0/MCU/ARM_GCC_Support/ewl/EWL_C/include/eof.h \
+ C:/Freescale/CW\ MCU\ v11.0/MCU/ARM_GCC_Support/ewl/EWL_C/include/va_list.h \
+ C:/Freescale/CW\ MCU\ v11.0/MCU/ARM_GCC_Support/ewl/EWL_C/include/file_struc.h \
+ C:/Freescale/CW\ MCU\ v11.0/MCU/ARM_GCC_Support/ewl/EWL_C/include/stdio_api.h \
+ C:/Freescale/CW\ MCU\ v11.0/MCU/ARM_GCC_Support/ewl/EWL_C/include/stdarg.h \
+ C:/Freescale/CW\ MCU\ v11.0/MCU/ARM_GCC_Support/ewl/EWL_C/include/cstdarg \
+ C:/Freescale/CW\ MCU\ v11.0/MCU/ARM_GCC_Support/ewl/EWL_C/include/arm/stdarg.ARM.h \
+ ../Generated_Code/IO1.h ../Generated_Code/MOSI.h \
+ ../Generated_Code/MISO.h ../Generated_Code/SCL.h ../Generated_Code/CS.h \
+ ../Generated_Code/I2C2.h \
+ D:/Git/Harvesters/EnergyHarvesting/Sources/Events.h \
+ D:/Git/Harvesters/EnergyHarvesting/Generated_Code/PE_Types.h \
+ D:/Git/Harvesters/EnergyHarvesting/Generated_Code/PE_Error.h \
+ D:/Git/Harvesters/EnergyHarvesting/Generated_Code/PE_Const.h \
+ D:/Git/Harvesters/EnergyHarvesting/Generated_Code/IO_Map.h \
+ D:/Git/Harvesters/EnergyHarvesting/Generated_Code/LED.h \
+ D:/Git/Harvesters/EnergyHarvesting/Generated_Code/WAIT1.h \
+ D:/Git/Harvesters/EnergyHarvesting/Generated_Code/CsIO1.h \
+ D:/Git/Harvesters/EnergyHarvesting/Generated_Code/IO1.h \
+ D:/Git/Harvesters/EnergyHarvesting/Generated_Code/MOSI.h \
+ D:/Git/Harvesters/EnergyHarvesting/Generated_Code/MISO.h \
+ D:/Git/Harvesters/EnergyHarvesting/Generated_Code/SCL.h \
+ D:/Git/Harvesters/EnergyHarvesting/Generated_Code/CS.h \
+ D:/Git/Harvesters/EnergyHarvesting/Generated_Code/I2C2.h
+
+C:/Freescale/CW\ MCU\ v11.0/MCU/ARM_GCC_Support/ewl/EWL_C/include/lib_ewl.prefix:
+
+C:/Freescale/CW\ MCU\ v11.0/MCU/ARM_GCC_Support/ewl/EWL_C/include/common.prefix:
+
+C:/Freescale/CW\ MCU\ v11.0/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewl_c_version.h:
+
+../Generated_Code/Cpu.h:
+
+../Generated_Code/PE_Types.h:
+
+C:/Freescale/CW\ MCU\ v11.0/MCU/ARM_GCC_Support/ewl/EWL_C/include/stdint.h:
+
+C:/Freescale/CW\ MCU\ v11.0/MCU/ARM_GCC_Support/ewl/EWL_C/include/ansi_parms.h:
+
+C:/Freescale/CW\ MCU\ v11.0/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewlGlobals.h:
+
+C:/Freescale/CW\ MCU\ v11.0/MCU/ARM_GCC_Support/ewl/EWL_C/include/arm/ansi_prefix.ARM.h:
+
+C:/Freescale/CW\ MCU\ v11.0/MCU/ARM_GCC_Support/ewl/EWL_C/include/os_enum.h:
+
+C:/Freescale/CW\ MCU\ v11.0/MCU/ARM_GCC_Support/ewl/EWL_C/include/cstdint:
+
+C:/Freescale/CW\ MCU\ v11.0/MCU/ARM_GCC_Support/ewl/EWL_C/include/limits_api.h:
+
+C:/Freescale/CW\ MCU\ v11.0/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewl_lib_ext1.h:
+
+../Generated_Code/PE_Error.h:
+
+../Generated_Code/PE_Const.h:
+
+../Generated_Code/IO_Map.h:
+
+D:/Git/Harvesters/EnergyHarvesting/Static_Code/IO_Map/MKL25Z4.h:
+
+../Generated_Code/LED.h:
+
+D:/Git/Harvesters/EnergyHarvesting/Static_Code/PDD/GPIO_PDD.h:
+
+D:/Git/Harvesters/EnergyHarvesting/Static_Code/PDD/PDD_Types.h:
+
+../Generated_Code/WAIT1.h:
+
+../Generated_Code/CsIO1.h:
+
+C:/Freescale/CW\ MCU\ v11.0/MCU/ARM_GCC_Support/ewl/EWL_C/include/stdio.h:
+
+C:/Freescale/CW\ MCU\ v11.0/MCU/ARM_GCC_Support/ewl/EWL_C/include/cstdio:
+
+C:/Freescale/CW\ MCU\ v11.0/MCU/ARM_GCC_Support/ewl/EWL_C/include/cstddef:
+
+C:/Freescale/CW\ MCU\ v11.0/MCU/ARM_GCC_Support/ewl/EWL_C/include/null.h:
+
+C:/Freescale/CW\ MCU\ v11.0/MCU/ARM_GCC_Support/ewl/EWL_C/include/size_t.h:
+
+C:/Freescale/CW\ MCU\ v11.0/MCU/ARM_GCC_Support/ewl/EWL_C/include/wchar_t.h:
+
+C:/Freescale/CW\ MCU\ v11.0/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewl_rsize_t.h:
+
+C:/Freescale/CW\ MCU\ v11.0/MCU/ARM_GCC_Support/ewl/EWL_C/include/eof.h:
+
+C:/Freescale/CW\ MCU\ v11.0/MCU/ARM_GCC_Support/ewl/EWL_C/include/va_list.h:
+
+C:/Freescale/CW\ MCU\ v11.0/MCU/ARM_GCC_Support/ewl/EWL_C/include/file_struc.h:
+
+C:/Freescale/CW\ MCU\ v11.0/MCU/ARM_GCC_Support/ewl/EWL_C/include/stdio_api.h:
+
+C:/Freescale/CW\ MCU\ v11.0/MCU/ARM_GCC_Support/ewl/EWL_C/include/stdarg.h:
+
+C:/Freescale/CW\ MCU\ v11.0/MCU/ARM_GCC_Support/ewl/EWL_C/include/cstdarg:
+
+C:/Freescale/CW\ MCU\ v11.0/MCU/ARM_GCC_Support/ewl/EWL_C/include/arm/stdarg.ARM.h:
+
+../Generated_Code/IO1.h:
+
+../Generated_Code/MOSI.h:
+
+../Generated_Code/MISO.h:
+
+../Generated_Code/SCL.h:
+
+../Generated_Code/CS.h:
+
+../Generated_Code/I2C2.h:
+
+D:/Git/Harvesters/EnergyHarvesting/Sources/Events.h:
+
+D:/Git/Harvesters/EnergyHarvesting/Generated_Code/PE_Types.h:
+
+D:/Git/Harvesters/EnergyHarvesting/Generated_Code/PE_Error.h:
+
+D:/Git/Harvesters/EnergyHarvesting/Generated_Code/PE_Const.h:
+
+D:/Git/Harvesters/EnergyHarvesting/Generated_Code/IO_Map.h:
+
+D:/Git/Harvesters/EnergyHarvesting/Generated_Code/LED.h:
+
+D:/Git/Harvesters/EnergyHarvesting/Generated_Code/WAIT1.h:
+
+D:/Git/Harvesters/EnergyHarvesting/Generated_Code/CsIO1.h:
+
+D:/Git/Harvesters/EnergyHarvesting/Generated_Code/IO1.h:
+
+D:/Git/Harvesters/EnergyHarvesting/Generated_Code/MOSI.h:
+
+D:/Git/Harvesters/EnergyHarvesting/Generated_Code/MISO.h:
+
+D:/Git/Harvesters/EnergyHarvesting/Generated_Code/SCL.h:
+
+D:/Git/Harvesters/EnergyHarvesting/Generated_Code/CS.h:
+
+D:/Git/Harvesters/EnergyHarvesting/Generated_Code/I2C2.h:
